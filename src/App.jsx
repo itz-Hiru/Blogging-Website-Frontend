1fe7a3a6b9b5f00de9b0ba8031d99109563a4c74
itz-Hiru <hirumithagithub@gmail.com>
2025-03-19 19:14:36 +0530

    Create navbar and add linked all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createContext, useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar.component";
+import SideNav from "./components/sidenavbar.component";
+import { lookInSession } from "./common/session";
+import Editor from "./pages/editor.pages";
+import HomePage from "./pages/home.page";
+import SearchPage from "./pages/search.page";
+import PageNotFound from "./pages/404.page";
+import ProfilePage from "./pages/profile.page";
+import BlogPage from "./pages/blog.page";
+import ChangePassword from "./pages/change-password.page";
+import EditProfile from "./pages/edit-profile.page";
+import Notifications from "./pages/notifications.page";
+import ManageBlogs from "./pages/manage-blogs.page";
+import UserAuthForm from "./pages/userAuthForm.page";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const UserContext = createContext({});
+
+export const ThemeContext = createContext({});
+
+const darkThemePreference = () =>
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const App = () => {
+  const [userAuth, setUserAuth] = useState({});
+
+  const [theme, setTheme] = useState(() =>
+    darkThemePreference() ? "dark" : "light"
+  );
+
+  useEffect(() => {
+    let userInSession = lookInSession("user");
+    let themeInSession = lookInSession("theme");
+
+    userInSession
+      ? setUserAuth(JSON.parse(userInSession))
+      : setUserAuth({ access_token: null });
+
+    if (themeInSession) {
+      setTheme(() => {
+        document.body.setAttribute("data-theme", themeInSession);
+
+        return themeInSession;
+      });
+    } else {
+      document.body.setAttribute("data-theme", theme);
+    }
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <UserContext.Provider value={{ userAuth, setUserAuth }}>
+        <Routes>
+          <Route path="/editor" element={<Editor />} />
+          <Route path="/editor/:blog_id" element={<Editor />} />
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<HomePage />} />
+            <Route path="dashboard" element={<SideNav />}>
+              <Route path="blogs" element={<ManageBlogs />} />
+              <Route path="notifications" element={<Notifications />} />
+            </Route>
+            <Route path="settings" element={<SideNav />}>
+              <Route path="edit-profile" element={<EditProfile />} />
+              <Route path="change-password" element={<ChangePassword />} />
+            </Route>
+            <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+            <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+            <Route path="search/:query" element={<SearchPage />} />
+            <Route path="user/:id" element={<ProfilePage />} />
+            <Route path="blog/:blog_id" element={<BlogPage />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
+        </Routes>
+      </UserContext.Provider>
+    </ThemeContext.Provider>
+  );
+};
 
-export default App
+export default App;
